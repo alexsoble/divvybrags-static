96097c60c4dd2940f7d027f31966dd676c8aba6e
alexsoble <asoble@gmail.com>
2013-12-26 14:17:25 -0600

    leaderboard link

---
+++ assets/leaderboard.js
@@ -0,0 +1,17 @@
+$(function() {
+  $.ajax({
+    type: "GET",
+    url: "http://divvybrags-leaderboard.herokuapp.com/entries.json", 
+    success: function(data) {
+      // console.log(data);
+      for (var i = 0; i <= data.length - 1; i++) {
+        var leaderboard_entry = data[i];
+        var leaderboard_position = Object.keys(leaderboard_entry)[0];
+        var name = leaderboard_entry[leaderboard_position]["name"];
+        var miles = leaderboard_entry[leaderboard_position]["miles"];
+        var entry_html = leaderboard_position + ". " + name + ": " + miles + "mi<br/>";
+        $('#leaderboard').append(entry_html);
+      }
+    }
+  });
+});
